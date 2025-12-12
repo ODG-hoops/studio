ee6975a4453b324ddd94e6a55fecc72e0ed75875
Sammy Phonical <phonical22@gmail.com>
2025-12-12 12:06:38 +0000

    yes change the font to times new roman

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['Times New Roman', 'serif'],
+        headline: ['Times New Roman', 'serif'],
         code: ['monospace'],
       },
       colors: {
